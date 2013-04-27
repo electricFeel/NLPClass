a6eb27d16342514ef23606b17434349a40a96506
Omar Ayub <omarayub@Omars-MacBook-Pro.local>
2013-04-27 13:28:30 -0400

    Began adding articles for MTurk

---
+++ articles.py
@@ -0,0 +1,26 @@
+#mississippi ricin arrest
+mississippi = ['http://www.cnn.com/2013/04/27/justice/ricin-investigation/index.html?hpt=hp_t1',
+			   'http://www.nytimes.com/2013/04/28/us/mississippi-man-arrested-in-sending-of-letters-laced-with-ricin.html?_r=0',
+			   'http://www.latimes.com/news/nationworld/nation/la-naw-ricin-suspect-arrested-20130427,0,5431467.story',
+			   'http://www.foxnews.com/us/2013/04/27/miss-man-arrested-in-investigation-into-poisoned-letters-sent-to-president/',
+			   'http://news.yahoo.com/mississippi-man-targeted-ricin-letters-investigation-arrested-145219248.html',
+			   'http://news.msn.com/crime-justice/mississippi-man-arrested-in-ricin-letters-probe',
+			   'http://www.miamiherald.com/2013/04/26/3366796/man-named-in-ricin-mailing-case.html',
+			   'http://www.washingtonpost.com/national/fbi-miss-man-arrested-in-investigation-into-poisoned-letters-sent-to-president-others/2013/04/27/64a7324a-af4d-11e2-b59e-adb43da03a8a_story.html',
+			   '',
+			   '',]
+
+#boston bomber miranda rights issue
+boston_miranda = ['http://www.foxnews.com/us/2013/04/25/officials-says-boston-bombing-suspect-described-terror-plot-before-miranda/',
+                  'http://www.cnn.com/2013/04/27/us/boston-attack/index.html?hpt=us_c2',
+                  'http://articles.latimes.com/2013/apr/25/nation/la-na-boston-bombing-20130426',
+                  'http://www.cbsnews.com/8301-201_162-57581382/ap-boston-marathon-bombing-suspect-dzhokhar-tsarnaev-silent-after-read-miranda-rights/',
+                  'http://www.washingtonpost.com/politics/courts_law/boston-honors-slain-mit-officer-investigators-want-to-talk-to-brothers-parents-in-russia/2013/04/24/be74738e-ad3e-11e2-a8e6-b6e4cc7c49d1_story.html',
+                  'http://news.msn.com/crime-justice/official-bombings-suspect-described-his-role-in-plot-before-miranda',
+                  '',
+                  '',
+                  '',
+                  '',
+                 ]
+
+
