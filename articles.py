5649649bbc4dd24d6958930975d11f9c97b88f41
Omar <omar.ayub@gmail.com>
2013-04-29 00:26:34 -0300

    Update articles.py
    fixed some spacing issues

--- articles.py
+++ articles.py
@@ -1,13 +1,13 @@
 #mississippi ricin arrest
 mississippi = ['http://www.cnn.com/2013/04/27/justice/ricin-investigation/index.html?hpt=hp_t1',
-		'http://www.nytimes.com/2013/04/28/us/mississippi-man-arrested-in-sending-of-letters-laced-with-ricin.html?_r=0',
-		'http://www.latimes.com/news/nationworld/nation/la-naw-ricin-suspect-arrested-20130427,0,5431467.story',
-		'http://www.foxnews.com/us/2013/04/27/miss-man-arrested-in-investigation-into-poisoned-letters-sent-to-president/',
-		'http://news.yahoo.com/mississippi-man-targeted-ricin-letters-investigation-arrested-145219248.html',
-		'http://news.msn.com/crime-justice/mississippi-man-arrested-in-ricin-letters-probe',
-		'http://www.miamiherald.com/2013/04/26/3366796/man-named-in-ricin-mailing-case.html',
-		'http://www.washingtonpost.com/national/fbi-miss-man-arrested-in-investigation-into-poisoned-letters-sent-to-president-others/2013/04/27/64a7324a-af4d-11e2-b59e-adb43da03a8a_story.html',
-	      ]
+			   'http://www.nytimes.com/2013/04/28/us/mississippi-man-arrested-in-sending-of-letters-laced-with-ricin.html?_r=0',
+		       'http://www.latimes.com/news/nationworld/nation/la-naw-ricin-suspect-arrested-20130427,0,5431467.story',
+		       'http://www.foxnews.com/us/2013/04/27/miss-man-arrested-in-investigation-into-poisoned-letters-sent-to-president/',
+		       'http://news.yahoo.com/mississippi-man-targeted-ricin-letters-investigation-arrested-145219248.html',
+		       'http://news.msn.com/crime-justice/mississippi-man-arrested-in-ricin-letters-probe',
+		       'http://www.miamiherald.com/2013/04/26/3366796/man-named-in-ricin-mailing-case.html',
+		       'http://www.washingtonpost.com/national/fbi-miss-man-arrested-in-investigation-into-poisoned-letters-sent-to-president-others/2013/04/27/64a7324a-af4d-11e2-b59e-adb43da03a8a_story.html',
+	          ]
 
 #boston bomber miranda rights issue
 boston_miranda = ['http://www.foxnews.com/us/2013/04/25/officials-says-boston-bombing-suspect-described-terror-plot-before-miranda/',
@@ -24,10 +24,10 @@ boston_miranda = ['http://www.foxnews.com/us/2013/04/25/officials-says-boston-bo
 
 #sunil tripathi (brown student found dead)
 sunil_tripathi = ['http://www.cnn.com/2013/04/25/us/rhode-island-missing-brown-student/index.html?hpt=hp_bn1',
-				  'http://www.nytimes.com/2013/04/26/us/sunil-tripathi-student-at-brown-is-found-dead.html?_r=0',
-				  'http://www.latimes.com/news/nation/nationnow/la-na-nn-sunil-tripathi-river-20130425,0,6230548.story',
-				  'http://www.cbsnews.com/8301-504083_162-57581387-504083/sunil-tripathi-update-body-found-in-waters-off-rhode-island-park-idd-as-brown-university-student-official-says/',
-				  'http://www.washingtonpost.com/national/officials-body-found-in-water-off-providence-park-that-of-missing-brown-student/2013/04/25/9b87b670-adb5-11e2-b240-9ef3a72c67cc_story.html',
+		  		  'http://www.nytimes.com/2013/04/26/us/sunil-tripathi-student-at-brown-is-found-dead.html?_r=0',
+		  		  'http://www.latimes.com/news/nation/nationnow/la-na-nn-sunil-tripathi-river-20130425,0,6230548.story',
+		  		  'http://www.cbsnews.com/8301-504083_162-57581387-504083/sunil-tripathi-update-body-found-in-waters-off-rhode-island-park-idd-as-brown-university-student-official-says/',
+		  		  'http://www.washingtonpost.com/national/officials-body-found-in-water-off-providence-park-that-of-missing-brown-student/2013/04/25/9b87b670-adb5-11e2-b240-9ef3a72c67cc_story.html',
 				  'http://news.yahoo.com/sunil-tripathi-dead-medias-false-boston-id-isnt-162512716.html',
 				  'http://news.msn.com/us/body-pulled-from-river-confirmed-as-brown-student',
 				  ]
