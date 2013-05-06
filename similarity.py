0218b427f998932d2e18c6055be4d3f9fac185cb
lucassa <lucas.sa@gmail.com>
2013-05-06 19:37:54 -0400

    abstracting tokenization, normalization, and similarity

--- similarity.py
+++ similarity.py
@@ -3,18 +3,48 @@ import numpy as np
 from nltk.tokenize.punkt import PunktSentenceTokenizer
 from sklearn.feature_extraction.text import TfidfTransformer, CountVectorizer
 
+# SETTINGS
+sent_tokenize_func = punktokenizer
+normalize_func = tfidf
+similarity_func = dotproduct
+
+
 #http://joshbohde.com/blog/document-summarization
 
 def textrank(document):
-    sentence_tokenizer = PunktSentenceTokenizer()
-    sentences = sentence_tokenizer.tokenize(document)
+    sentences = sent_tokenize_func(document)
 
-    bow_matrix = CountVectorizer().fit_transform(sentences)
-    normalized = TfidfTransformer().fit_transform(bow_matrix)
+    matrix = CountVectorizer().fit_transform(sentences)
 
-    similarity_graph = normalized * normalized.T
+    # normalize counts
+    normalized = normalize_func(matrix)
+
+    # creates a similarity graph matrix
+    similarity_graph = similarity_func(normalized)
 
     nx_graph = nx.from_scipy_sparse_matrix(similarity_graph)
+
+    # use PageRank to score vertices in the graph
     scores = nx.pagerank(nx_graph)
+
+    # return vertices ordered by score
     return sorted(((scores[i], s) for i, s in enumerate(sentences)),
                   reverse=True)
+
+
+# Sentence Tokenizer functions
+def punktokenizer(document):
+    sentence_tokenizer = PunktSentenceTokenizer()
+    return sentence_tokenizer.tokenize(document)
+
+
+# Normalization functions
+
+def tfidf(matrix):
+    return TfidfTransformer().fit_transform(matrix)
+
+
+# Similarity functions
+
+def dotproduct(matrix):
+    return matrix * matrix.T
