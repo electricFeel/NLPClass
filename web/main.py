b42fd4fb8dceb9cbb3fccc58ae4ea6f69bc5929a
lucassa <lucas.sa@gmail.com>
2013-05-07 17:08:19 -0400

    getting info from dev set

--- web/main.py
+++ web/main.py
@@ -1,17 +1,21 @@
 import sys
 from bottle import route, run, template, view, request, response, static_file
 from os import path
+import pickle
 
 # adding main project path
 sys.path.append(path.join(path.dirname(__file__), "../"))
 
 import twitter
 import articles
-from algorithm_test import Topic
+from algorithm_test import Topic, Document
 
 # getting current twitter trends and caching
 trend_topics = twitter.get_trends()
 
+# getting dev and eval sets
+topics = pickle.load(open("../dev_set.p", "rb"))
+print topics
 
 @route('/')
 @view('topic')
@@ -19,29 +23,6 @@ def index():
     ret = dict()
     ret['trend_topics'] = trend_topics
     return ret
-
-
-# TEMP: fake result
-result = {
-    'topic': 'Starbucks Poisoning',
-    'sentences': [
-                    {
-                    'sentence': 'A woman has been arrested after she allegedly tried to sneak tainted bottles of orange juice into a refrigerator at a Starbucks coffee shop in San Jose.',
-                    'from_article': 'http://www.latimes.com/local/lanow/la-me-ln-woman-tainted-juice-starbucks-20130430,0,362185.story'
-                    },
-                    { 
-                    'sentence': 'Behbehanian could make her first appearance in court as soon as Thursday, authorities said.',
-                    'from_article': 'http://www.usatoday.com/story/money/business/2013/04/30/tainted-orange-juice/2125579/'
-                    }
-                ],
-    'articles':
-          ['http://www.usatoday.com/story/money/business/2013/04/30/tainted-orange-juice/2125579/',
-          'http://www.cnn.com/2013/04/30/justice/california-starbucks-tainted-juice/index.html',
-          'http://www.cbsnews.com/8301-504083_162-57582174-504083/poisoned-o.j-placed-in-calif-starbucks-woman-arrested-police-say/',
-          'http://abcnews.go.com/blogs/headlines/2013/04/poisoned-oj-slipped-into-starbucks-cold-case/',
-          'http://www.latimes.com/local/lanow/la-me-ln-woman-tainted-juice-starbucks-20130430,0,362185.story']
-}
-
 
 @route('/topic/<name>')
 @route('/topic')
@@ -53,13 +34,14 @@ def topic(name=None):
         else:
             name = "starbucks"  # example query
 
-    print name
-    #print articles.total
     # TODO: query our system and receive result
-    if name in articles.total:
-        topic = Topic(name)
-        for url in articles.total[name]:
-            topic.add_document(url=url)
+
+    topic = None
+    for top in topics:
+        if top.topic == name:
+            topic = top
+
+    if topic:
         topic.summarize()
         result = {
             'topic': topic.topic,
