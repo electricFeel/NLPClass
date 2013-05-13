c6fc46badd7679f521bf03054af7909ca7e3d4fe
lucastsa <lucas.sa@gmail.com>
2013-05-13 18:55:56 -0400

    adding cosine similarity measure

--- similarity.py
+++ similarity.py
@@ -2,6 +2,7 @@ import networkx as nx
 import numpy as np
 from nltk.tokenize.punkt import PunktSentenceTokenizer
 from sklearn.feature_extraction.text import TfidfTransformer, CountVectorizer
+from sklearn.metrics.pairwise import pairwise_distances, pairwise_kernels
 from collections import namedtuple
 
 #http://joshbohde.com/blog/document-summarization
@@ -23,7 +24,7 @@ def textrank(document):
     # creates a similarity graph matrix
     similarity_graph = similarity_func(normalized)
 
-    nx_graph = nx.from_scipy_sparse_matrix(similarity_graph)
+    nx_graph = nx.to_networkx_graph(similarity_graph)
 
     # use PageRank to score vertices in the graph
     scores = nx.pagerank(nx_graph)
@@ -52,7 +53,16 @@ def dotproduct(matrix):
     return matrix * matrix.T
 
 
+def cosine(matrix):
+    return pairwise_kernels(matrix, metric='cosine')
+
+
+def linear(matrix):
+    return pairwise_kernels(matrix)
+
+
 # SETTINGS
 sent_tokenize_func = punktokenizer
 normalize_func = tfidf
-similarity_func = dotproduct
+similarity_func = cosine
+
